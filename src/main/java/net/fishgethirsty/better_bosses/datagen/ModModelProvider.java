46efd7cbc1919edff80be46826f25e3fa7d62f32
F1shg3th1rsty <bsullenger27@gmail.com>
2025-03-26 17:17:05 -0500

    Blue Moss finally has its look right. Also, it is added manually and the same will happen with all  complicated models like this when data gen does not have it.

--- src/main/java/net/fishgethirsty/better_bosses/datagen/ModModelProvider.java
+++ src/main/java/net/fishgethirsty/better_bosses/datagen/ModModelProvider.java
@@ -14,7 +14,7 @@ public class ModModelProvider extends FabricModelProvider {
 
     @Override
     public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
-        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.BLUE_MOSS);
+
     }
 
     @Override
